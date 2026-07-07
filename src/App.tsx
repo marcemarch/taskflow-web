a2522a535ba38616259b39cb02bb74b2de382277
Marcelo <march.aruquipa@gmail.com>
2026-07-07 19:17:24 -0400

    feat: inicializar Vite + React + TypeScript + Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() { 
+  return ( 
+  <div className="min-h-screen bg-slate-900 flex items-center justify-center"> 
+  < h1 className="text-4xl font-bold text-emerald-400">
+   ✅ Tailwind v4 funcionando 
+   </h1> 
+   </div> 
+   );
+  } 
+  export default App;
